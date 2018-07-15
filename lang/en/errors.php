419d87d07b9d1345242b2ef062efca3d198af5c4
Michael Papin <febalist@gmail.com>
2018-07-15 22:01:03 +0000

    Apply fixes from StyleCI

--- lang/en/errors.php
+++ lang/en/errors.php
@@ -2,8 +2,8 @@
 
 return [
     'error_code' => 'Error code',
-    '403' => 'Access denied',
-    '404' => 'Resource not found',
-    '500' => 'Webservice currently unavailable',
-    '503' => 'Webservice currently unavailable',
+    '403'        => 'Access denied',
+    '404'        => 'Resource not found',
+    '500'        => 'Webservice currently unavailable',
+    '503'        => 'Webservice currently unavailable',
 ];
